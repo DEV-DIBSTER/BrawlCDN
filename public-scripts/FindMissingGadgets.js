11a5a89e1a5996f041a40c405ee8ea6ec071f268
DIBSTERYT <76603072+DIBSTERYT@users.noreply.github.com>
2023-01-24 19:04:04 -0500

    feat: Open sourcing update scripts.

---
+++ public-scripts/FindMissingGadgets.js
@@ -0,0 +1,60 @@
+const Axios = require('axios');
+const Fs = require('fs');
+const Config = require('../configuration/config.json');
+const https = require('https');
+
+const AsktoDownload = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+Axios({
+    url: "https://bsproxy.royaleapi.dev/v1/brawlers",
+    headers: {
+        "Authorization": "Bearer " + Config.Token
+    }
+}).then(async (Response) => {
+    if(Response.status != 200) return;
+
+    const GadgetIcons = await Fs.readdirSync('./assets/Gadgets').map(File => `${File.replace('.png', '')}`);
+    const GadgetCirlceIcons = await Fs.readdirSync('./assets/Gadgets-Circles').map(File => `${File.replace('.png', '')}`);
+
+    let GadgetsFromGame = [];
+    await Response.data.items.map(Brawler => Brawler.gadgets).forEach(m => m.forEach(e => GadgetsFromGame.push(e.id)));
+
+    const NonDuplicates1 = await GadgetsFromGame.filter(x => !GadgetIcons.includes(`${x}`));
+    const NonDuplicates2 = await GadgetsFromGame.filter(x => !GadgetCirlceIcons.includes(`${x}`));
+
+    console.log('Normal Gadgets:');
+    console.log(NonDuplicates1);
+    console.log('Circle Gadgets:');
+    console.log(NonDuplicates2);
+
+    if(NonDuplicates1.length != 0){
+        AsktoDownload.question('Would you like to try and download the Gadgets? Please respond with Yes or No: ', (YesOrNo) => {
+            if(YesOrNo.toLocaleLowerCase() == 'yes'){
+
+                Promise.all(NonDuplicates1.map(async(Gadgets) => {
+                    const FileLocation = Fs.createWriteStream(`./assets/Gadgets/${Gadgets}.png`);
+                    https.get(`https://cdn.brawlstats.com/gadgets/${Gadgets}.png`, function (Response) {
+                        Response.pipe(FileLocation);
+
+                        // After download finish.
+                        FileLocation.on("finish", () => {
+                            FileLocation.close();
+                            console.log(`Download File Complete: ${Gadgets}.png`);
+                        });
+                    });
+                })).then(Response => {
+                    AsktoDownload.close();
+                });
+            } else if (YesOrNo.toLowerCase() == 'no'){
+                console.log('File download is skipped.');
+
+                AsktoDownload.close();
+            };
+          });
+    }  else {
+        console.log('No files to download!');
+    };
+});
---
+++ public-scripts/FindMissingStarPowers.js
@@ -0,0 +1,60 @@
+const Axios = require('axios');
+const Fs = require('fs');
+const Config = require('../configuration/config.json');
+const https = require('https');
+
+const AsktoDownload = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+Axios({
+    url: "https://bsproxy.royaleapi.dev/v1/brawlers",
+    headers: {
+        "Authorization": "Bearer " + Config.Token
+    }
+}).then(async (Response) => {
+    if(Response.status != 200) return;
+
+    const StarPowerIcons = await Fs.readdirSync('./assets/StarPowers').map(File => `${File.replace('.png', '')}`);
+    const StarPowesCirlceIcons = await Fs.readdirSync('./assets/StarPowers-Circles').map(File => `${File.replace('.png', '')}`);
+
+    let StarPowersFromGame = [];
+    await Response.data.items.map(Brawler => Brawler.starPowers).forEach(m => m.forEach(e => StarPowersFromGame.push(e.id)));
+
+    const NonDuplicates1 = StarPowersFromGame.filter(x => !StarPowerIcons.includes(`${x}`));
+    const NonDuplicates2 = StarPowersFromGame.filter(x => !StarPowesCirlceIcons.includes(`${x}`));
+
+    console.log('Normal Star Powers:');
+    console.log(NonDuplicates1);
+    console.log('Circle Star Powers:');
+    console.log(NonDuplicates2);
+
+    if(NonDuplicates1.length != 0){
+        AsktoDownload.question('Would you like to try and download the StarPowers? Please respond with Yes or No: ', (YesOrNo) => {
+            if(YesOrNo.toLocaleLowerCase() == 'yes'){
+
+                Promise.all(NonDuplicates1.map(async(StarPowers) => {
+                    const FileLocation = Fs.createWriteStream(`./assets/StarPowers/${StarPowers}.png`);
+                    https.get(`https://cdn.brawlstats.com/star-powers/${StarPowers}.png`, function (Response) {
+                        Response.pipe(FileLocation);
+
+                        // After download finish.
+                        FileLocation.on("finish", () => {
+                            FileLocation.close();
+                            console.log(`Download File Complete: ${StarPowers}.png`);
+                        });
+                    });
+                })).then(Response => {
+                    AsktoDownload.close();
+                });
+            } else if (YesOrNo.toLowerCase() == 'no'){
+                console.log('File download is skipped.');
+
+                AsktoDownload.close();
+            };
+          });
+    } else {
+        console.log('No files to download!');
+    };
+});
